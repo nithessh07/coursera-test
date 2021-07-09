4a06d78a4a0b3327628561aedd769b9d2372bb02
nitheesh <snktiger.19@gmail.com>
2021-07-10 01:52:44 +0530

    hello

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 (function () {
 'use strict';
 
-var  x="hello";
 
 angular.module('myFirstApp',[])
 
-.controller('MyFirstContoller', function($scope){
+.controller('MyFirstContoller', function ($scope){
   $scope.name="nitheesh";
 $scope.sayHello=function (){
   return "hello";
